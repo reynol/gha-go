b39a5af791fb95194ec3b7ccc3338482850c4739
Reynol Zacapala <reynol@gmail.com>
2023-08-23 18:47:36 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+	"io"
+    "os"
+    "os/exec"
+    "path/filepath"
+)
+
+func main() {
+    // Leer argumentos de línea de comandos
+    args := os.Args[1:]
+
+    // Verificar que se haya proporcionado un argumento
+    if len(args) == 0 {
+        fmt.Println("Debe proporcionar el nombre del script a ejecutar")
+        os.Exit(1)
+    }
+
+    // Obtener el nombre del script a ejecutar
+    scriptName := args[0]
+    scriptPath := filepath.Join(".", scriptName, "script.go")
+
+    // Verificar si el script existe
+    if _, err := os.Stat(scriptPath); os.IsNotExist(err) {
+        fmt.Println("Script not found:", scriptPath)
+        return
+    }
+
+	fmt.Println("Executing script:", scriptPath)
+    // Ejecutar el script correspondiente
+    cmd := exec.Command("go", "run", scriptPath)
+    stdout, err := cmd.StdoutPipe()
+
+    if err != nil {
+        fmt.Println("Error al obtener la salida estándar del proceso:", err)
+        os.Exit(1)
+    }
+
+    if err := cmd.Start(); err != nil {
+        fmt.Println("Error al ejecutar el script:", err)
+        os.Exit(1)
+    }
+
+    // Copiar la salida estándar del proceso secundario a la salida estándar del proceso principal
+    if _, err := io.Copy(os.Stdout, stdout); err != nil {
+        fmt.Println("Error al copiar la salida estándar del proceso:", err)
+        os.Exit(1)
+    }
+
+    if err := cmd.Wait(); err != nil {
+        fmt.Println("Error al esperar a que el proceso termine:", err)
+        os.Exit(1)
+    }
+}
---
+++ script2/script.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "encoding/csv"
+    "fmt"
+    "os"
+	"io"
+)
+
+func main() {
+    // Open the file
+    file, err := os.Open("./script2/file.csv")
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return
+    }
+
+    // Create a CSV reader
+    reader := csv.NewReader(file)
+
+    // Read the rows
+    for {
+        record, err := reader.Read()
+        if err == io.EOF {
+            break
+        }
+
+        // Print the row
+        for _, value := range record {
+            fmt.Println(value)
+        }
+    }
+}
